d6646cf56aacb7ec55f429c75a3a93e2d90e9d75
yitzy299 <yitzy299@noreply.github.com>
2021-08-11 16:40:50 +0100

    Cleanup
    Signed-off-by: yitzy299 <yitzy299@noreply.github.com>

--- src/main/kotlin/com/github/quiltservertools/ledger/commands/CommandConsts.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/commands/CommandConsts.kt
@@ -3,5 +3,4 @@ package com.github.quiltservertools.ledger.commands
 object CommandConsts {
     const val PARAMS = "params"
     const val PERMISSION_LEVEL = 3
-    const val FOUR = 4
 }
--- src/main/kotlin/com/github/quiltservertools/ledger/config/SearchSpec.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/config/SearchSpec.kt
@@ -1,9 +1,8 @@
 package com.github.quiltservertools.ledger.config
 
-import com.github.quiltservertools.ledger.commands.CommandConsts
 import com.uchuhimo.konf.ConfigSpec
 
 object SearchSpec : ConfigSpec() {
     val pageSize by required<Int>()
-    val purgePermissionLevel by optional(CommandConsts.FOUR)
+    val purgePermissionLevel by required<Int>()
 }
