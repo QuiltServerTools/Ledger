cc8e25688ac28c6494c9bca17c969b52cba1d4f7
yitzy299 <yitzy299@noreply.github.com>
2021-08-13 19:21:09 +0100

    Log flint and steel usage
    Signed-off-by: yitzy299 <yitzy299@noreply.github.com>

---
+++ src/main/java/com/github/quiltservertools/ledger/mixin/FlintAndSteelItemMixin.java
@@ -0,0 +1,23 @@
+package com.github.quiltservertools.ledger.mixin;
+
+import com.github.quiltservertools.ledger.callbacks.PlayerBlockPlaceCallback;
+import net.minecraft.item.FlintAndSteelItem;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FlintAndSteelItem.class)
+public class FlintAndSteelItemMixin {
+    @Inject(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", shift = At.Shift.AFTER))
+    public void logFlintAndSteelUse(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+        BlockPos pos = context.getBlockPos().offset(context.getSide());
+        if (context.getPlayer() != null) {
+            PlayerBlockPlaceCallback.Companion.getEVENT().invoker().place(context.getWorld(), context.getPlayer(), pos, context.getWorld().getBlockState(pos), new ItemPlacementContext(context), null);
+        }
+    }
+}
