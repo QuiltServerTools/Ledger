f409cf5602ed342f9e3947b703985ba7b8e2410e
yitzy299 <66269211+yitzy299@users.noreply.github.com>
2021-08-13 19:33:09 +0100

    Make class abstract because why not
    Co-authored-by: Potatoboy99999 <51728317+PotatoPresident@users.noreply.github.com>

--- src/main/java/com/github/quiltservertools/ledger/mixin/FlintAndSteelItemMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/FlintAndSteelItemMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FlintAndSteelItem.class)
-public class FlintAndSteelItemMixin {
+public abstract class FlintAndSteelItemMixin {
     @Inject(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", shift = At.Shift.AFTER))
     public void logFlintAndSteelUse(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
         BlockPos pos = context.getBlockPos().offset(context.getSide());
