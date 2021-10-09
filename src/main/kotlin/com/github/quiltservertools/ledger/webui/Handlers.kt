4549675b4dc2b9ec92c0445bf4ab008e58d6cda1
yitzy299 <yitzy299@users.noreply.github.com>
2021-10-09 18:11:32 +0100

    Add overview page
    Signed-off-by: yitzy299 <yitzy299@users.noreply.github.com>

--- src/main/kotlin/com/github/quiltservertools/ledger/webui/Handlers.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/webui/Handlers.kt
@@ -1,10 +1,13 @@
 package com.github.quiltservertools.ledger.webui
 
+import com.github.quiltservertools.ledger.actions.ActionType
 import com.github.quiltservertools.ledger.database.DatabaseManager
 import io.javalin.http.Context
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.future.future
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 object Handlers {
     @OptIn(DelicateCoroutinesApi::class)
@@ -12,9 +15,38 @@ object Handlers {
     fun handleOverview(ctx: Context) {
         ctx.future(
             GlobalScope.future {
-                //TODO this is broken for some reason
-                return@future DatabaseManager.getDashboardActions(12).toSet()
+                val results = DatabaseManager.getDashboardActions(12).toSet()
+                return@future results.map {
+                    SentAction.fromActionType(it)
+                }
             }
         )
+    }
+}
+
+data class SentAction(
+    val identifier: String,
+    var time: String,
+    var x: Int,
+    var y: Int,
+    var z: Int,
+    var world: String,
+    var objectString: String,
+    var oldObjectString: String,
+    var sourceName: String,
+    var sourceProfile: String
+) {
+    companion object {
+        fun fromActionType(action: ActionType): SentAction {
+            val objectIdentifier = action.objectIdentifier.toString()
+            val oldObjectIdentifier = action.objectIdentifier.toString()
+            val time = action.timestamp.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"))
+            val pos = action.pos
+            val world = action.world.toString()?: ""
+            val identifier = action.identifier
+            val sourceName = action.sourceName
+            val sourceProfile = action.sourceProfile?.name?: ""
+            return SentAction(identifier, time, pos.x, pos.y, pos.z, world, objectIdentifier, oldObjectIdentifier, sourceName, sourceProfile)
+        }
     }
 }
--- src/main/resources/vue/views/dashboard.vue
+++ src/main/resources/vue/views/dashboard.vue
@@ -1,10 +1,29 @@
 <!--suppress JSAnnotator -->
 <template id="dashboard">
-    <ul class="action-overview-list">
-        <li v-for="action in actions">
-            <a :href="`/actions/${action.identifier}`">{{action.identifier}}</a>
-        </li>
-    </ul>
+    <table class="table table-light mx-auto" style="width: 75%;">
+        <thead>
+        <tr>
+            <th scope="col">Time</th>
+            <th scope="col">World</th>
+            <th scope="col">Position</th>
+            <th scope="col">Action Type</th>
+            <th scope="col">Source</th>
+            <th scope="col">Object</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="action in actions">
+            <td>{{action.time}}</td>
+            <td>{{action.world}}</td>
+            <td>{{action.x}} {{action.y}} {{action.z}}</td>
+            <td>{{action.identifier}}</td>
+            <td v-if="action.sourceProfile != null">{{action.sourceProfile}}</td>
+            <td v-else>{{action.sourceName}}</td>
+            <td v-if="action.objectString != null">{{action.objectString}}</td>
+            <td v-else>{{action.oldObjectString}}</td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 <script>
 Vue.component("dashboard", {
