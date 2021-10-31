2a3e2ab2554f1775cd2114b046bee3ecaa7c0e7e
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2021-10-31 08:42:12 -0700

    Add worlds to world table when instead of just at startup (#109)
    Fixes support for runtime worlds

--- src/main/kotlin/com/github/quiltservertools/ledger/listeners/WorldEventListener.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/listeners/WorldEventListener.kt
@@ -1,10 +1,14 @@
 package com.github.quiltservertools.ledger.listeners
 
+import com.github.quiltservertools.ledger.Ledger
 import com.github.quiltservertools.ledger.actionutils.ActionFactory
 import com.github.quiltservertools.ledger.callbacks.ItemInsertCallback
 import com.github.quiltservertools.ledger.callbacks.ItemRemoveCallback
 import com.github.quiltservertools.ledger.database.DatabaseManager
+import kotlinx.coroutines.launch
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents
 import net.minecraft.item.ItemStack
+import net.minecraft.server.MinecraftServer
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
@@ -12,6 +16,13 @@ import net.minecraft.util.math.BlockPos
 fun registerWorldEventListeners() {
     ItemInsertCallback.EVENT.register(::onItemInsert)
     ItemRemoveCallback.EVENT.register(::onItemRemove)
+    ServerWorldEvents.LOAD.register(::onWorldLoad)
+}
+
+fun onWorldLoad(server: MinecraftServer, world: ServerWorld) {
+    Ledger.launch {
+        DatabaseManager.registerWorld(world.registryKey.value)
+    }
 }
 
 private fun onItemRemove(
