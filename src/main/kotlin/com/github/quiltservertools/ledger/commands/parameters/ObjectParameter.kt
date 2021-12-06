73f71065e6e9d3d35dcfe963b2378ce8f045b2b8
PotatoPresident <51728317+PotatoPresident@users.noreply.github.com>
2021-12-06 09:58:42 -0800

    Started adding tag support

--- src/main/kotlin/com/github/quiltservertools/ledger/commands/parameters/ObjectParameter.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/commands/parameters/ObjectParameter.kt
@@ -7,28 +7,45 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder
 import net.minecraft.command.CommandSource
 import net.minecraft.command.argument.IdentifierArgumentType
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.tag.BlockTags
+import net.minecraft.tag.EntityTypeTags
+import net.minecraft.tag.ItemTags
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 import java.util.concurrent.CompletableFuture
 
-class ObjectParameter : SimpleParameter<Identifier>() {
-    private val identifiers = mutableListOf<Identifier>()
-
-    init {
-        identifiers.addAll(Registry.ITEM.ids)
-        identifiers.addAll(Registry.BLOCK.ids)
-        identifiers.addAll(Registry.ENTITY_TYPE.ids)
+class ObjectParameter : SimpleParameter<List<Identifier>>() {
+    private val identifiers = mutableListOf<Identifier>().apply {
+        addAll(Registry.ITEM.ids)
+        addAll(Registry.BLOCK.ids)
+        addAll(Registry.ENTITY_TYPE.ids)
+    }
+    private val tags = mutableListOf<Identifier>().apply {
+        addAll(BlockTags.getTagGroup().tagIds)
+        addAll(ItemTags.getTagGroup().tagIds)
+        addAll(EntityTypeTags.getTagGroup().tagIds)
     }
 
-    override fun parse(stringReader: StringReader): Identifier = IdentifierArgumentType.identifier().parse(stringReader)
+    override fun parse(stringReader: StringReader): List<Identifier> {
+        if (stringReader.string.isEmpty()) return listOf()
+        if (stringReader.string[stringReader.cursor] == '#') stringReader.skip()
+        return listOf(IdentifierArgumentType.identifier().parse(stringReader))
+    }
 
     override fun getSuggestions(
-        context: CommandContext<ServerCommandSource>?,
-        builder: SuggestionsBuilder?
+        context: CommandContext<ServerCommandSource>,
+        builder: SuggestionsBuilder
     ): CompletableFuture<Suggestions> {
-        return CommandSource.suggestIdentifiers(
-            identifiers,
-            builder
-        )
+        return if (builder.remaining.startsWith("#")) {
+            CommandSource.suggestIdentifiers(
+                BlockTags.getTagGroup().tagIds,
+                builder.createOffset(builder.start + 1)
+            )
+        } else {
+            CommandSource.suggestIdentifiers(
+                identifiers,
+                builder
+            )
+        }
     }
 }
