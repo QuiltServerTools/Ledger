b882b09641be17b3cce5abd0efb5e6919dc8d75d
Altirix <19439141+Altirix@users.noreply.github.com>
2021-12-12 21:39:24 +0000

    move cauldron stuff to own folder fix dripstone and precipitate logs

--- src/main/java/com/github/quiltservertools/ledger/mixin/blocks/cauldron/CauldronBehaviorMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/blocks/cauldron/CauldronBehaviorMixin.java
@@ -1,4 +1,4 @@
-package com.github.quiltservertools.ledger.mixin;
+package com.github.quiltservertools.ledger.mixin.blocks.cauldron;
 
 import com.github.quiltservertools.ledger.LedgerKt;
 import com.github.quiltservertools.ledger.callbacks.BlockChangeCallback;
@@ -21,7 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.function.Predicate;
 
 @Mixin(CauldronBehavior.class)
-public interface CauldronBehaviorMixin {
+public interface CauldronBehaviorMixin{
 
     @Inject(method = "fillCauldron",at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"))
             private static void ledgerLogFillCauldron(World world, BlockPos pos, PlayerEntity player, Hand hand, ItemStack stack, BlockState state,
@@ -43,6 +43,7 @@ public interface CauldronBehaviorMixin {
         LedgerKt.logInfo("leather clean");
         //ledgerLogDrainCauldron(world, pos, state, player);
     }
+
     @Inject(method = "method_32214",at = @At(value = "INVOKE",
             target = "Lnet/minecraft/block/LeveledCauldronBlock;decrementFluidLevel(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V",
             shift = At.Shift.AFTER))
@@ -57,14 +58,14 @@ public interface CauldronBehaviorMixin {
     private static void ledgerLogShulkerDrainCauldron(BlockState state, World world, BlockPos pos,
                                                       PlayerEntity player, Hand hand, ItemStack stack, CallbackInfoReturnable<ActionResult> cir) {
         LedgerKt.logInfo("shulker clean");
-       // ledgerLogDrainCauldron(world, pos, state, player);
+       // ledgerLogDrainCauldron(world, pos, state, player)
     }
     @Inject(method = "method_32219",at = @At(value = "INVOKE",
             target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z",
             shift = At.Shift.AFTER))
     private static void ledgerLogBottleFillWaterCauldron(BlockState state, World world, BlockPos pos,
                                                          PlayerEntity player, Hand hand, ItemStack stack, CallbackInfoReturnable<ActionResult> cir) {
-        LedgerKt.logInfo("water bottle bruh");
+        LedgerKt.logInfo("water bottle on water cauldron");
         ledgerLogFillCauldron(world, pos, state,world.getBlockState(pos), player);
     }
     @Inject(method = "method_32222",at = @At(value = "INVOKE",
@@ -72,7 +73,7 @@ public interface CauldronBehaviorMixin {
             shift = At.Shift.AFTER))
     private static void ledgerLogBottleFillEmptyCauldron(BlockState state, World world, BlockPos pos,
                                                          PlayerEntity player, Hand hand, ItemStack stack, CallbackInfoReturnable<ActionResult> cir) {
-        LedgerKt.logInfo("water bottle");
+        LedgerKt.logInfo("water bottle on empty cauldron");
         ledgerLogFillCauldron(world, pos, state,world.getBlockState(pos), player);
     }
     @Inject(method = "method_32220",at = @At(value = "INVOKE",
--- src/main/java/com/github/quiltservertools/ledger/mixin/blocks/cauldron/CauldronBlockMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/blocks/cauldron/CauldronBlockMixin.java
@@ -1,19 +1,14 @@
-package com.github.quiltservertools.ledger.mixin.blocks;
+package com.github.quiltservertools.ledger.mixin.blocks.cauldron;
 
 import com.github.quiltservertools.ledger.LedgerKt;
 import com.github.quiltservertools.ledger.callbacks.BlockChangeCallback;
 import com.github.quiltservertools.ledger.utility.Sources;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.CauldronBlock;
 import net.minecraft.block.LeveledCauldronBlock;
-import net.minecraft.block.cauldron.CauldronBehavior;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
@@ -21,16 +16,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.function.Predicate;
-
-import static net.minecraft.block.LeveledCauldronBlock.LEVEL;
 
 @Mixin(CauldronBlock.class)
 public abstract class CauldronBlockMixin {
 
-    @Inject(method = "fillFromDripstone",at = @At(value = "TAIL"))
+    @Inject(method = "fillFromDripstone",at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z",shift = At.Shift.AFTER))
     private void ledgerLogIncrementLevelCauldron(BlockState state, World world, BlockPos pos, Fluid fluid, CallbackInfo ci) {
         LedgerKt.logInfo("fillFromDripstone");
         BlockChangeCallback.EVENT.invoker().changeBlock(
@@ -43,7 +34,8 @@ public abstract class CauldronBlockMixin {
                 Sources.FILL_DRIP);
     }
 
-    @Inject(method = "precipitationTick",at = @At(value = "TAIL"))
+    @Inject(method = "precipitationTick",at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z",shift = At.Shift.AFTER))
     private void ledgerLogIncrementLevelCauldron(BlockState state, World world, BlockPos pos, Biome.Precipitation precipitation, CallbackInfo ci) {
         LedgerKt.logInfo("precipitationTick");
         BlockChangeCallback.EVENT.invoker().changeBlock(
@@ -55,4 +47,5 @@ public abstract class CauldronBlockMixin {
                 null,
                 Sources.FILL_RAIN_SNOW);
     }
+
 }
