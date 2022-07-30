c2cc351909a18f5ae762f9dcd0f6df97e4695d8f
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2022-07-29 18:54:10 -0700

    Update client testmod to 1.19 (#181)

--- src/testmod/kotlin/com/github/quiltservertools/ledger/testmod/commands/SearchCommand.kt
+++ src/testmod/kotlin/com/github/quiltservertools/ledger/testmod/commands/SearchCommand.kt
@@ -1,11 +1,11 @@
 package com.github.quiltservertools.ledger.testmod.commands
 
+import com.github.quiltservertools.ledger.testmod.LedgerTest
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.tree.LiteralCommandNode
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.argument
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal
-import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource
-import com.github.quiltservertools.ledger.testmod.LedgerTest
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 
 object SearchCommand {
     fun build(): LiteralCommandNode<FabricClientCommandSource> =
