67fd473b7318cdc349d1deaa324dd036755f1619
nikijaz <nikijaz.dev@gmail.com>
2022-08-31 17:39:12 +0300

    Added logging of beds broken as a result of player interaction

--- src/main/java/com/github/quiltservertools/ledger/mixin/blocks/BedBlockMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/blocks/BedBlockMixin.java
@@ -1,9 +1,12 @@
 package com.github.quiltservertools.ledger.mixin.blocks;
 
 import com.github.quiltservertools.ledger.callbacks.BlockBreakCallback;
+import com.github.quiltservertools.ledger.utility.Sources;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,5 +20,11 @@ public abstract class BedBlockMixin {
     public void logBedBreak(Args args, World world, BlockPos pos, BlockState state, PlayerEntity player) {
         BlockPos blockPos = args.get(0);
         BlockBreakCallback.EVENT.invoker().breakBlock(world, blockPos, world.getBlockState(blockPos), world.getBlockEntity(blockPos), player);
+    }
+
+    @ModifyArgs(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;removeBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"))
+    public void logBedExplosion(Args args, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        BlockPos blockPos = args.get(0);
+        BlockBreakCallback.EVENT.invoker().breakBlock(world, blockPos, world.getBlockState(blockPos), world.getBlockEntity(blockPos), Sources.INTERACT, player);
     }
 }
