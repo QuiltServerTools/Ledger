ab354e8138a5a60947dcfa547bd03212d58cb209
nikijaz <nikijaz.dev@gmail.com>
2022-08-31 16:54:13 +0300

    Added logging of blocks broken by the ender dragon

---
+++ src/main/java/com/github/quiltservertools/ledger/mixin/entities/EnderDragonEntityMixin.java
@@ -0,0 +1,22 @@
+package com.github.quiltservertools.ledger.mixin.entities;
+
+import com.github.quiltservertools.ledger.callbacks.BlockBreakCallback;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(EnderDragonEntity.class)
+public abstract class EnderDragonEntityMixin {
+    @ModifyArgs(method = "destroyBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;removeBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"))
+    private void logEnderDragonBreakingBlocks(Args args) {
+        EnderDragonEntity entity = (EnderDragonEntity) (Object) this;
+        World world = entity.getEntityWorld();
+        BlockPos blockPos = args.get(0);
+        BlockBreakCallback.EVENT.invoker().breakBlock(world, blockPos, world.getBlockState(blockPos), world.getBlockEntity(blockPos), Registry.ENTITY_TYPE.getId(entity.getType()).getPath());
+    }
+}
