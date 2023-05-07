bfee3a207c662955e09bb9c82a865be27eb916a2
nikijaz <nikijaz.dev@gmail.com>
2023-05-07 19:54:51 +0300

    Removed unnecessary LocalCapture

--- src/main/java/com/github/quiltservertools/ledger/mixin/entities/VillagerEntityMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/entities/VillagerEntityMixin.java
@@ -3,19 +3,17 @@ package com.github.quiltservertools.ledger.mixin.entities;
 import com.github.quiltservertools.ledger.callbacks.EntityKillCallback;
 import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.WitchEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(VillagerEntity.class)
 public abstract class VillagerEntityMixin {
-    @Inject(method = "onStruckByLightning", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private void ledgerVillagerToWitch(ServerWorld world, LightningEntity lightning, CallbackInfo ci, WitchEntity witchEntity) {
+    @Inject(method = "onStruckByLightning", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V"))
+    private void ledgerVillagerToWitch(ServerWorld world, LightningEntity lightning, CallbackInfo ci) {
         LivingEntity entity = (LivingEntity) (Object) this;
         EntityKillCallback.EVENT.invoker().kill(entity.world, entity.getBlockPos(), entity, world.getDamageSources().lightningBolt());
     }
