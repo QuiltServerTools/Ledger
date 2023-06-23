056e0f172f27c93a11c77ffcfcdbee3319f748ce
Drex <nicknamedrex@gmail.com>
2023-06-23 12:30:07 +0200

    Replace mixins with `@Inject` and `@Local`

--- src/main/java/com/github/quiltservertools/ledger/mixin/BucketDispenserBehaviorMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/BucketDispenserBehaviorMixin.java
@@ -3,26 +3,31 @@ package com.github.quiltservertools.ledger.mixin;
 import com.github.quiltservertools.ledger.callbacks.BlockBreakCallback;
 import com.github.quiltservertools.ledger.callbacks.BlockChangeCallback;
 import com.github.quiltservertools.ledger.utility.Sources;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.FluidDrainable;
 import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "net/minecraft/block/dispenser/DispenserBehavior$9")
 public abstract class BucketDispenserBehaviorMixin extends ItemDispenserBehavior {
-    @WrapOperation(method = "dispenseSilently(Lnet/minecraft/util/math/BlockPointer;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FluidDrainable;tryDrainFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Lnet/minecraft/item/ItemStack;"))
-    private ItemStack logFluidPickup(FluidDrainable instance, WorldAccess worldAccess, BlockPos pos, BlockState blockState, Operation<ItemStack> original, BlockPointer pointer, ItemStack stack) {
+
+    @Inject(
+            method = "dispenseSilently(Lnet/minecraft/util/math/BlockPointer;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/FluidDrainable;tryDrainFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Lnet/minecraft/item/ItemStack;",
+                    shift = At.Shift.AFTER
+            )
+    )
+    private void logFluidPickup(BlockPointer pointer, ItemStack stack, CallbackInfoReturnable<ItemStack> cir, @Local ItemStack itemStack, @Local BlockPos pos, @Local BlockState blockState) {
         var world = pointer.getWorld();
-        var oldBlockEntity = world.getBlockEntity(pos);
-        var itemStack = original.call(instance, worldAccess, pos, blockState);
         if (!itemStack.isEmpty()) {
             if (blockState.isLiquid() || blockState.isOf(Blocks.POWDER_SNOW)) {
                 BlockBreakCallback.EVENT.invoker().breakBlock(world, pos, blockState, world.getBlockEntity(pos), Sources.REDSTONE);
@@ -32,12 +37,12 @@ public abstract class BucketDispenserBehaviorMixin extends ItemDispenserBehavior
                         pos,
                         blockState,
                         world.getBlockState(pos),
-                        oldBlockEntity,
+                        world.getBlockEntity(pos),
                         world.getBlockEntity(pos),
                         Sources.REDSTONE
                 );
             }
         }
-        return itemStack;
     }
+
 }
--- src/main/java/com/github/quiltservertools/ledger/mixin/BucketItemMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/BucketItemMixin.java
@@ -4,30 +4,24 @@ import com.github.quiltservertools.ledger.callbacks.BlockBreakCallback;
 import com.github.quiltservertools.ledger.callbacks.BlockChangeCallback;
 import com.github.quiltservertools.ledger.callbacks.BlockPlaceCallback;
 import com.github.quiltservertools.ledger.utility.Sources;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.FluidDrainable;
-import net.minecraft.block.FluidFillable;
 import net.minecraft.block.Waterloggable;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.FluidState;
 import net.minecraft.item.BucketItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BucketItem.class)
@@ -44,66 +38,57 @@ public abstract class BucketItemMixin {
         }
     }
     
-    @Inject(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+    @Inject(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BucketItem;playEmptyingSound(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;)V", ordinal = 1))
     private void logFluidPlace(@Nullable PlayerEntity player, World world, BlockPos pos, @Nullable BlockHitResult hitResult, CallbackInfoReturnable<Boolean> cir) {
-        var blockstate = world.getBlockState(pos);
-        if (!blockstate.getFluidState().isStill()) {
-            if (player != null) {
-                BlockPlaceCallback.EVENT.invoker().place(world, pos, this.fluid.getDefaultState().getBlockState(), null, player);
-            } else {
-                BlockPlaceCallback.EVENT.invoker().place(world, pos, this.fluid.getDefaultState().getBlockState(), null, Sources.REDSTONE);
-            }
+        if (player != null) {
+            BlockPlaceCallback.EVENT.invoker().place(world, pos, this.fluid.getDefaultState().getBlockState(), null, player);
+        } else {
+            BlockPlaceCallback.EVENT.invoker().place(world, pos, this.fluid.getDefaultState().getBlockState(), null, Sources.REDSTONE);
         }
     }
-    
-    @Redirect(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FluidFillable;tryFillWithFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/fluid/FluidState;)Z"))
-    private boolean logWaterlog(FluidFillable instance, WorldAccess worldAccess, BlockPos pos, BlockState blockState, FluidState fluidState, @Nullable PlayerEntity player, World world) {
-        var oldBlockEntity = world.getBlockEntity(pos);
-        boolean success = instance.tryFillWithFluid(world, pos, blockState, ((FlowableFluid)this.fluid).getStill(false));
-        if (success) {
-            if (player != null) BlockChangeCallback.EVENT.invoker().changeBlock(
+
+    @Inject(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BucketItem;playEmptyingSound(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;)V", ordinal = 0))
+    private void logWaterlog(PlayerEntity player, World world, BlockPos pos, BlockHitResult hitResult, CallbackInfoReturnable<Boolean> cir, @Local BlockState blockState) {
+        if (player != null) {
+            BlockChangeCallback.EVENT.invoker().changeBlock(
                     world,
                     pos,
                     blockState,
                     world.getBlockState(pos),
-                    oldBlockEntity,
+                    world.getBlockEntity(pos),
                     world.getBlockEntity(pos),
                     player
             );
-            else 
-                BlockChangeCallback.EVENT.invoker().changeBlock(
+        } else {
+            BlockChangeCallback.EVENT.invoker().changeBlock(
                     world,
                     pos,
                     blockState,
                     world.getBlockState(pos),
-                    oldBlockEntity,
+                    world.getBlockEntity(pos),
                     world.getBlockEntity(pos),
                     Sources.REDSTONE
             ); //TODO This is dumb. Make some sort of Source wrapper
         }
-        return success;
     }
-    
-    @WrapOperation(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FluidDrainable;tryDrainFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Lnet/minecraft/item/ItemStack;"))
-    private ItemStack logFluidPickup(FluidDrainable instance, WorldAccess worldAccess, BlockPos pos, BlockState blockState, Operation<ItemStack> original, World world, PlayerEntity player, Hand hand) {
-        var oldBlockEntity = world.getBlockEntity(pos);
-        var itemStack = original.call(instance, worldAccess, pos, blockState);
-        if (!itemStack.isEmpty()) {
-            if (blockState.getBlock() instanceof Waterloggable) {
-                BlockChangeCallback.EVENT.invoker().changeBlock(
-                        world,
-                        pos,
-                        blockState,
-                        world.getBlockState(pos),
-                        oldBlockEntity,
-                        world.getBlockEntity(pos),
-                        player
-                );
-            } else {
-                BlockBreakCallback.EVENT.invoker().breakBlock(world, pos, blockState, world.getBlockEntity(pos), player);
 
-            }
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;incrementStat(Lnet/minecraft/stat/Stat;)V"))
+    private void logFluidPickup(World world, PlayerEntity player, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir, @Local(ordinal = 0) BlockPos pos, @Local BlockState blockState) {
+        if (blockState.getBlock() instanceof Waterloggable) {
+            BlockChangeCallback.EVENT.invoker().changeBlock(
+                    world,
+                    pos,
+                    blockState,
+                    world.getBlockState(pos),
+                    world.getBlockEntity(pos),
+                    world.getBlockEntity(pos),
+                    player
+            );
+        } else {
+            BlockBreakCallback.EVENT.invoker().breakBlock(world, pos, blockState, world.getBlockEntity(pos), player);
         }
-        return itemStack;
+
     }
+
+
 }
