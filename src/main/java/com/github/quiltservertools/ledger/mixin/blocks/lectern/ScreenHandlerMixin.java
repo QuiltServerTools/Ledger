22f64da8aaaeac92c04c4eada2d3065ebe7c67eb
nikijaz <nikijaz.dev@gmail.com>
2023-07-03 13:13:47 +0300

    Fixed a possible memory leak with lectern logging

---
+++ src/main/java/com/github/quiltservertools/ledger/mixin/blocks/lectern/ScreenHandlerMixin.java
@@ -0,0 +1,20 @@
+package com.github.quiltservertools.ledger.mixin.blocks.lectern;
+
+import com.github.quiltservertools.ledger.utility.PlayerLecternHook;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.LecternScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ScreenHandler.class)
+public class ScreenHandlerMixin {
+    @Inject(method = "onClosed", at = @At(value = "INVOKE"))
+    public void onClosed(PlayerEntity player, CallbackInfo ci) {
+        if (player.currentScreenHandler instanceof LecternScreenHandler) {
+            PlayerLecternHook.getActiveHandlers().remove(player);
+        }
+    }
+}
