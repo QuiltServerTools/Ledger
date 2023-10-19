27584dc58e3d80236c2bfdddd4df8c5b2a21d39b
Genau6502 <66269211+Genau6502@users.noreply.github.com>
2023-10-19 20:03:58 +0100

    Filter search by rollback status
    Adds a new param (rolledBack) to allow filter by rollback status. This param is ignored in the query builder unless set. Fixes #195

--- src/main/kotlin/com/github/quiltservertools/ledger/actionutils/ActionSearchParams.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/actionutils/ActionSearchParams.kt
@@ -10,16 +10,18 @@ data class ActionSearchParams(
     val bounds: BlockBox?,
     val before: Instant?,
     val after: Instant?,
+    val rolledBack: Boolean?,
     var actions: MutableSet<Negatable<String>>?,
     var objects: MutableSet<Negatable<Identifier>>?,
     var sourceNames: MutableSet<Negatable<String>>?,
     var sourcePlayerIds: MutableSet<Negatable<UUID>>?,
-    var worlds: MutableSet<Negatable<Identifier>>?,
+    var worlds: MutableSet<Negatable<Identifier>>?
 ) {
     private constructor(builder: Builder) : this(
         builder.bounds,
         builder.before,
         builder.after,
+        builder.rolledBack,
         builder.actions,
         builder.objects,
         builder.sourceNames,
@@ -27,7 +29,7 @@ data class ActionSearchParams(
         builder.worlds
     )
 
-    fun isEmpty() = listOf(bounds, before, after, actions, objects, sourceNames, sourcePlayerIds, worlds).all { it == null }
+    fun isEmpty() = listOf(bounds, before, after, actions, objects, sourceNames, sourcePlayerIds, worlds, rolledBack).all { it == null }
 
     companion object {
         inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
@@ -37,6 +39,7 @@ data class ActionSearchParams(
         var bounds: BlockBox? = null
         var before: Instant? = null
         var after: Instant? = null
+        var rolledBack: Boolean? = null
         var actions: MutableSet<Negatable<String>>? = null
         var objects: MutableSet<Negatable<Identifier>>? = null
         var sourceNames: MutableSet<Negatable<String>>? = null
