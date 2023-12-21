b3923638b29aa209e7cea699d2a89620b6809a6c
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2023-12-20 18:50:51 -0800

    Slight cleanup

--- src/main/kotlin/com/github/quiltservertools/ledger/utility/TextColorPallet.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/utility/TextColorPallet.kt
@@ -2,14 +2,19 @@ package com.github.quiltservertools.ledger.utility
 
 import com.github.quiltservertools.ledger.config.ColorSpec
 import com.github.quiltservertools.ledger.config.config
+import com.mojang.serialization.DataResult
 import net.minecraft.text.Style
 import net.minecraft.text.TextColor
 
 @Suppress("MagicNumber")
 object TextColorPallet {
-    val primary: Style get() =  Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.primary]).get().left().orElse(null))
-    val primaryVariant: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.primaryVariant]).get().left().orElse(null))
-    val secondary: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.secondary]).get().left().orElse(null))
-    val secondaryVariant: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.secondaryVariant]).get().left().orElse(null))
-    val light: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.light]).get().left().orElse(null))
+    val primary: Style
+        get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.primary]).getOrNull())
+
+    val primaryVariant: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.primaryVariant]).getOrNull())
+    val secondary: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.secondary]).getOrNull())
+    val secondaryVariant: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.secondaryVariant]).getOrNull())
+    val light: Style get() = Style.EMPTY.withColor(TextColor.parse(config[ColorSpec.light]).getOrNull())
 }
+
+fun DataResult<TextColor>.getOrNull(): TextColor? = this.get().left().orElse(null)
