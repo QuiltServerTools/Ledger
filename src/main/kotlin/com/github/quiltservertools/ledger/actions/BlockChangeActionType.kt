e00d0497b171973173652ea2822ea8a506e33438
TheDeathlyCow <thedeathlycow@gmail.com>
2024-04-04 19:01:11 -1000

    mark block entity for update on rollback

--- src/main/kotlin/com/github/quiltservertools/ledger/actions/BlockChangeActionType.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/actions/BlockChangeActionType.kt
@@ -23,7 +23,9 @@ open class BlockChangeActionType : AbstractActionType() {
     override fun rollback(server: MinecraftServer): Boolean {
         val world = server.getWorld(world)
         world?.setBlockState(pos, oldBlockState())
+
         world?.getBlockEntity(pos)?.readNbt(StringNbtReader.parse(extraData))
+        world?.chunkManager?.markForUpdate(pos)
 
         return true
     }
