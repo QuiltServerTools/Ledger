9dd82833616046a60a2ff497c17893264710edf5
Liyan Zhao <return65535@qq.com>
2024-06-28 14:33:30 +0800

    fix

--- src/main/java/com/github/quiltservertools/ledger/mixin/ScreenHandlerMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/ScreenHandlerMixin.java
@@ -4,22 +4,21 @@ import com.github.quiltservertools.ledger.callbacks.ItemInsertCallback;
 import com.github.quiltservertools.ledger.callbacks.ItemRemoveCallback;
 import com.github.quiltservertools.ledger.utility.HandledSlot;
 import com.github.quiltservertools.ledger.utility.HandlerWithContext;
+import com.github.quiltservertools.ledger.utility.ItemData;
 import com.github.quiltservertools.ledger.utility.Sources;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import net.minecraft.component.ComponentChanges;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -32,7 +31,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ScreenHandler.class)
 public abstract class ScreenHandlerMixin implements HandlerWithContext {
-    Map<Pair<Item, ComponentChanges>, Integer> changedStacks = HashMap.newHashMap(19);
+    Map<ItemData, Integer> changedStacks = HashMap.newHashMap(19);
     @Unique
     private ServerPlayerEntity player = null;
     
@@ -68,7 +67,10 @@ public abstract class ScreenHandlerMixin implements HandlerWithContext {
     private void ledgerCloseScreenLogChanges(PlayerEntity player, CallbackInfo ci) {
         if (!player.getWorld().isClient) {
             for (var pair : changedStacks.keySet()) {
-                ItemStack stack = new ItemStack(pair.getLeft().getRegistryEntry(), 1, pair.getRight());
+                ItemStack stack = new ItemStack(Registries.ITEM.getEntry(pair.getItem()), 1, pair.getChanges());
+                if (stack.isEmpty()) {
+                    continue;
+                }
                 int count = changedStacks.get(pair);
                 int countAbs = Math.abs(count);
                 List<ItemStack> splitStacks = new ArrayList<>();
@@ -111,33 +113,23 @@ public abstract class ScreenHandlerMixin implements HandlerWithContext {
     public void onStackChanged(@NotNull ItemStack old, @NotNull ItemStack itemStack, @NotNull BlockPos pos) {
         if (old.isEmpty() && !itemStack.isEmpty()) {
             // Add item
-            var key = new Pair<>(itemStack.getItem(), itemStack.getComponentChanges());
+            var key = new ItemData(itemStack.getItem(), itemStack.getComponentChanges());
             if (changedStacks.containsKey(key)) {
-                changedStacks.put(key, changedStacks.get(key) + 1);
+                changedStacks.put(key, changedStacks.get(key) + itemStack.getCount());
             } else {
-                changedStacks.put(key, 1);
+                changedStacks.put(key, itemStack.getCount());
             }
         } else if (!old.isEmpty() && itemStack.isEmpty()) {
             // Remove item
-            var key = new Pair<>(itemStack.getItem(), itemStack.getComponentChanges());
+            var key = new ItemData(old.getItem(), old.getComponentChanges());
             if (changedStacks.containsKey(key)) {
-                changedStacks.put(key, changedStacks.get(key) - 1);
+                changedStacks.put(key, changedStacks.get(key) - old.getCount());
             } else {
-                changedStacks.put(key, -1);
+                changedStacks.put(key, -old.getCount());
             }
         } else {
-            // Change item
-            var key = new Pair<>(itemStack.getItem(), itemStack.getComponentChanges());
-            if (changedStacks.containsKey(key)) {
-                changedStacks.put(key, changedStacks.get(key) - 1);
-            } else {
-                changedStacks.put(key, -1);
-            }
-            if (changedStacks.containsKey(key)) {
-                changedStacks.put(key, changedStacks.get(key) + 1);
-            } else {
-                changedStacks.put(key, 1);
-            }
+            onStackChanged(old, ItemStack.EMPTY, pos);
+            onStackChanged(ItemStack.EMPTY, itemStack, pos);
         }
     }
 }
