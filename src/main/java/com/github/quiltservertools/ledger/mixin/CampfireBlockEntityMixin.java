cb895f2aef886353d1f0e71b36323c0c619fd27a
Drex <nicknamedrex@gmail.com>
2024-10-03 01:48:04 +0200

    Update to 24w40a

--- src/main/java/com/github/quiltservertools/ledger/mixin/CampfireBlockEntityMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/CampfireBlockEntityMixin.java
@@ -4,8 +4,11 @@ import com.github.quiltservertools.ledger.callbacks.BlockChangeCallback;
 import com.github.quiltservertools.ledger.utility.Sources;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.CampfireBlockEntity;
+import net.minecraft.recipe.CampfireCookingRecipe;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.recipe.input.SingleStackRecipeInput;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +19,7 @@ public abstract class CampfireBlockEntityMixin {
 
     @Inject(method = "litServerTick", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/util/ItemScatterer;spawn(Lnet/minecraft/world/World;DDDLnet/minecraft/item/ItemStack;)V", shift = At.Shift.AFTER))
-    private static void logCampfireRemoveItem(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, CallbackInfo ci) {
+    private static void logCampfireRemoveItem(ServerWorld world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, RecipeManager.MatchGetter<SingleStackRecipeInput, CampfireCookingRecipe> matchGetter, CallbackInfo ci) {
         BlockChangeCallback.EVENT.invoker().changeBlock(world, pos, state, world.getBlockState(pos), campfire, world.getBlockEntity(pos), Sources.REMOVE);
     }
 
