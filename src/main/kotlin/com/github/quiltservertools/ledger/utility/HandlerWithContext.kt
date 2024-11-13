09faba9ca26f2ef5de50bd4f7aac7806fcdebea2
Liyan Zhao <return65535@qq.com>
2024-11-13 08:40:23 +0800

    fix: Only log item changes on screen closed to make it less spam (#275)
    Co-authored-by: Drex <nicknamedrex@gmail.com>

--- src/main/kotlin/com/github/quiltservertools/ledger/utility/HandlerWithContext.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/utility/HandlerWithContext.kt
@@ -1,5 +1,6 @@
 package com.github.quiltservertools.ledger.utility
 
+import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.math.BlockPos
 
@@ -7,4 +8,5 @@ interface HandlerWithContext {
     fun getPlayer(): ServerPlayerEntity?
     fun getPos(): BlockPos?
     fun setPos(pos: BlockPos)
+    fun onStackChanged(old: ItemStack, new: ItemStack, pos: BlockPos)
 }
