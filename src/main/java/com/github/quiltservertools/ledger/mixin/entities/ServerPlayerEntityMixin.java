e115efaa5eab51b57775751ca982f8bcbcd52dfd
Drex <nicknamedrex@gmail.com>
2025-03-25 23:52:37 +0100

    Fix NullPointerException

--- src/main/java/com/github/quiltservertools/ledger/mixin/entities/ServerPlayerEntityMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/entities/ServerPlayerEntityMixin.java
@@ -1,11 +1,9 @@
 package com.github.quiltservertools.ledger.mixin.entities;
 
 import com.github.quiltservertools.ledger.callbacks.ItemDropCallback;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,6 +14,9 @@ public abstract class ServerPlayerEntityMixin {
     @Inject(method = "dropItem", at = @At("RETURN"))
     private void logPlayerItemDrop(ItemStack stack, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> cir) {
         PlayerEntity player = (PlayerEntity) (Object) this;
-        ItemDropCallback.EVENT.invoker().drop(cir.getReturnValue(), player);
+        var itemEntity = cir.getReturnValue();
+        if (itemEntity != null) {
+            ItemDropCallback.EVENT.invoker().drop(itemEntity, player);
+        }
     }
 }
