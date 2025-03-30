2743587821ea67c04b9be6b5575e6b77515cad1d
MeiNanziiii <106649669+MeiNanziiii@users.noreply.github.com>
2025-03-30 23:12:51 +0300

    Updated gradle script, Ukrainian & Belarusian translation, some 1.21 blocks logging, smaller jar size, etc. (#301)
    * updated gradle script + minekord compat
    
    * updated deps & fixed java.lang.IllegalStateException: com.github.quiltservertools.ledger.database.Tables$Players.id is not in record set
    
    * use MigrationUtils instead of SchemaUtils
    
    * mixin improvements
    
    * DatabaseManager refactor (just because i don't like warning)
    
    * updated Ukrainian translation
    
    * button and decorated pot logging
    
    * detect -> detekt in libs.versions.toml
    
    * Belarusian and Latin variant
    
    ---------
    
    Co-authored-by: Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>

---
+++ src/main/java/com/github/quiltservertools/ledger/mixin/blocks/ButtonBlockMixin.java
@@ -0,0 +1,36 @@
+package com.github.quiltservertools.ledger.mixin.blocks;
+
+import com.github.quiltservertools.ledger.callbacks.BlockChangeCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ButtonBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(ButtonBlock.class)
+public abstract class ButtonBlockMixin {
+    @Unique
+    private PlayerEntity activePlayer;
+
+    @Inject(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/ButtonBlock;powerOn(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;)V"))
+    public void logButtonClick(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        activePlayer = player;
+    }
+
+    @ModifyArgs(method = "powerOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+    public void logLeverUse(Args args, BlockState state, World world, BlockPos pos, PlayerEntity playerEntity) {
+        if (activePlayer == null) return;
+
+        BlockState newState = args.get(1);
+        BlockChangeCallback.EVENT.invoker().changeBlock(world, pos, state, newState, null, null, activePlayer);
+    }
+}
--- src/main/java/com/github/quiltservertools/ledger/mixin/entities/ItemFrameEntityMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/entities/ItemFrameEntityMixin.java
@@ -12,7 +12,6 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
