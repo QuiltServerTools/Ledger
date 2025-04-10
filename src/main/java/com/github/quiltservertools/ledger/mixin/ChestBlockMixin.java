b1a1dcec06e1a80c67e8beb6b6469ce5914b21d5
Drex <nicknamedrex@gmail.com>
2025-04-10 04:38:20 +0200

    Fix double chest logging (#314)

---
+++ src/main/java/com/github/quiltservertools/ledger/mixin/ChestBlockMixin.java
@@ -0,0 +1,29 @@
+package com.github.quiltservertools.ledger.mixin;
+
+import com.github.quiltservertools.ledger.utility.HandlerWithContext;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.ScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net/minecraft/block/ChestBlock$2$1")
+public abstract class ChestBlockMixin {
+    @Shadow
+    ChestBlockEntity field_17358;
+
+    @Inject(
+            method = "createMenu(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/screen/ScreenHandler;",
+            at = @At("RETURN")
+    )
+    private void addPositionContext(int i, PlayerInventory playerInventory, PlayerEntity playerEntity, CallbackInfoReturnable<ScreenHandler> cir) {
+        ScreenHandler screenHandler = cir.getReturnValue();
+        if (screenHandler != null) {
+            ((HandlerWithContext) screenHandler).setPos(this.field_17358.getPos());
+        }
+    }
+}
