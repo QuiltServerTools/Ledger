3593b1e93078d34cb9d26f5082d10c2136e34cd2
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-11-11 21:13:07 +0100

    Fix crash when pushing water under lily pad.

--- src/main/java/com/github/quiltservertools/ledger/mixin/LilyPadBreakageMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/LilyPadBreakageMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.VehicleEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,10 +21,12 @@ public abstract class LilyPadBreakageMixin {
     private void ledgerModifyLilyPadBreakArgs(BlockPos pos, boolean drop, Entity entity, int maxUpdateDepth, CallbackInfoReturnable<Boolean> cir, @Local BlockState state) {
         if (!state.getBlock().equals(Blocks.LILY_PAD)) return;
         World world = (World) (Object) this;
-        if (entity.getFirstPassenger() instanceof PlayerEntity player) {
-            BlockBreakCallback.EVENT.invoker().breakBlock(world, pos, state, null, Sources.VEHICLE, player);
-        } else {
-            BlockBreakCallback.EVENT.invoker().breakBlock(world, pos, state, null, Sources.VEHICLE);
+        if (entity instanceof VehicleEntity) {
+            if (entity.getFirstPassenger() instanceof PlayerEntity player) {
+                BlockBreakCallback.EVENT.invoker().breakBlock(world, pos, state, null, Sources.VEHICLE, player);
+            } else {
+                BlockBreakCallback.EVENT.invoker().breakBlock(world, pos, state, null, Sources.VEHICLE);
+            }
         }
     }
 }
